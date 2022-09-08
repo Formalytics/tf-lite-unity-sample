411d656afc79530e916eee3d673f5c9dc0a8619a
Steve Dunford <81156188+steve-dunford@users.noreply.github.com>
2022-09-08 17:03:59 +0100

    Fix: Move main-thread-specific operations out of BaseImagePredictor constructor (#5)
    * Fix: Move model loading out of base image predictor
    
    * Instantiate TextureToTensor outside of BaseImagePredictor

--- Packages/com.github.asus4.tflite.common/Runtime/BaseImagePredictor.cs
+++ Packages/com.github.asus4.tflite.common/Runtime/BaseImagePredictor.cs
@@ -49,7 +49,7 @@ namespace TensorFlowLite
             set => resizeOptions = value;
         }
 
-        public BaseImagePredictor(string modelPath, Accelerator accelerator)
+        public BaseImagePredictor(byte[] model, TextureToTensor textureToTensor, Accelerator accelerator)
         {
             var options = new InterpreterOptions();
 
@@ -82,7 +82,7 @@ namespace TensorFlowLite
 
             try
             {
-                interpreter = new Interpreter(FileUtil.LoadFile(modelPath), options);
+                interpreter = new Interpreter(model, options);
             }
             catch (System.Exception e)
             {
@@ -108,7 +108,7 @@ namespace TensorFlowLite
                 interpreter.AllocateTensors();
             }
 
-            tex2tensor = new TextureToTensor();
+            tex2tensor = textureToTensor;
             resizer = new TextureResizer();
             resizeOptions = new TextureResizer.ResizeOptions()
             {
